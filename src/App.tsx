f58abf281dc6da122a871dd84fdc730d2197cbcb
Guanghui Wang <guanghuiw@vmware.com>
2021-09-16 09:23:03 +0800

    tmp: make localhost running for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import CSPHeader from './components/CSPHeader';
-import CSPTokenSwitcher from './components/CSPTokenSwitcher';
 import Test from './components/Test';
 
-import http from './utils/axios';
-
 function App() {
-  useEffect(() => {
-    http.get('/core/current-user').then((res) => {
-      console.log(res);
-    });
-    return () => {};
-  }, []);
   return (
     <div className='main-container'>
       <CSPHeader />
-      <Test />
+
       <div className='content-container'>
-        <div className='content-area'>PCDL Welcome Page for Anonymous Users.</div>
+        <div className='content-area'>
+          Icon inside Test component doesn't show correctly. The next line always shows a loading icon.
+          <Test />
+          <p>If CSPHeader is commented out, and refresh the page, the icon will display.</p>
+          <p>How does CSP header affect the icon? Why it doesn't in your project?</p>
+          <p>
+            csp-header-x seems not have a Shadow DOM, is this on purpose? How to avoid polluting the application without
+            Shadow DOM?
+          </p>
+          <p>If upgrading clr/react from v4 to cds/react v5.5.3 (latest), UI will be broken.</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { CdsIcon } from '@clr/react/icon';
 import { ClarityIcons, cogIcon } from '@clr/core/icon';
@@ -6,10 +6,6 @@ import { ClarityIcons, cogIcon } from '@clr/core/icon';
 ClarityIcons.addIcons(cogIcon);
 
 export default function Test() {
-  useEffect(() => {
-    return () => {};
-  }, []);
-
   return (
     <div>
       <CdsIcon shape='cog' />
